a7a6ea7d423e342ee57d35a2dc14d1dfa960caa9
padukadafa <padukadafa@gmail.com>
2025-05-18 00:22:54 +0700

    update avatar di daftar member komunitas

--- lib/features/community/presentation/pages/community_member/community_member_page.dart
+++ lib/features/community/presentation/pages/community_member/community_member_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:jeka/common/widgets/avatars/avatar.dart';
+import 'package:jeka/common/widgets/avatars/user_avatar.dart';
 import 'package:jeka/common/widgets/reuseable_text.dart';
 import 'package:jeka/di.dart';
 import 'package:jeka/features/community/domain/usecases/get_community_members_usecase.dart';
@@ -17,94 +18,99 @@ class CommunityMemberPage extends StatelessWidget {
         child: BlocBuilder<CommunityBloc, CommunityState>(
           builder: (context, state) {
             return Column(
-                  children: [
-                    Container(
-                      width: double.maxFinite,
-                      margin: const EdgeInsets.all(12),
-                      padding: const EdgeInsets.all(16),
-                      decoration: BoxDecoration(
-                        color: colorScheme.surfaceBright,
-                        boxShadow: [
-                          BoxShadow(
-                            color: colorScheme.shadow,
-                            blurRadius: 4,
-                          ),
-                        ],
+              children: [
+                Container(
+                  width: double.maxFinite,
+                  margin: const EdgeInsets.all(12),
+                  padding: const EdgeInsets.all(16),
+                  decoration: BoxDecoration(
+                    color: colorScheme.surfaceBright,
+                    boxShadow: [
+                      BoxShadow(
+                        color: colorScheme.shadow,
+                        blurRadius: 4,
                       ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const ReuseableText(
-                            "Organizer",
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          Column(
-                            children: (state.community?.members.where((val) => val.role != "member") ?? []).map(
-                              (member) {
-                                return  ListTile(
-                                  minLeadingWidth: 0,
-                                  contentPadding: EdgeInsets.all(0),
-                                  leading: Avatar(
-                                    size: 35,
-                                  ),
-                                  title: Text(member.name ?? ""),
-                                  trailing: ReuseableText(
-                                    member.role ?? "Admin",
-                                    fontSize: 14,
-                                  ),
-                                );
-                              },
-                            ).toList(),
-                          ),
-                        ],
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const ReuseableText(
+                        "Organizer",
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
                       ),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.all(12),
-                      padding: const EdgeInsets.all(16),
-                      width: double.maxFinite,
-                      decoration: BoxDecoration(
-                        color: colorScheme.surfaceBright,
-                        boxShadow: [
-                          BoxShadow(
-                            color: colorScheme.shadow,
-                            blurRadius: 4,
-                          ),
-                        ],
+                      Column(
+                        children: (state.community?.members
+                                    .where((val) => val.role != "member") ??
+                                [])
+                            .map(
+                          (member) {
+                            return ListTile(
+                              minLeadingWidth: 0,
+                              contentPadding: EdgeInsets.all(0),
+                              leading: UserAvatar(
+                                uid: member.userId,
+                                size: 35,
+                              ),
+                              title: Text(member.name ?? ""),
+                              trailing: ReuseableText(
+                                member.role ?? "Admin",
+                                fontSize: 14,
+                              ),
+                            );
+                          },
+                        ).toList(),
                       ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const ReuseableText(
-                            "Member",
-                            fontSize: 18,
-                            fontWeight: FontWeight.bold,
-                          ),
-                          Column(
-                            children: (state.community?.members
-                                        .where((val) => val.role == "member") ??
-                                    [])
-                                .map(
-                              (member) {
-                                return  ListTile(
-                                  leading: Avatar(
-                                    size: 35,
-                                  ),
-                                  title: Text(member.name ?? ""),
-                                  trailing: ReuseableText(
-                                    member.role ??"Member",
-                                    fontSize: 14,
-                                  ),
-                                );
-                              },
-                            ).toList(),
-                          ),
-                        ],
+                    ],
+                  ),
+                ),
+                Container(
+                  margin: const EdgeInsets.all(12),
+                  padding: const EdgeInsets.all(16),
+                  width: double.maxFinite,
+                  decoration: BoxDecoration(
+                    color: colorScheme.surfaceBright,
+                    boxShadow: [
+                      BoxShadow(
+                        color: colorScheme.shadow,
+                        blurRadius: 4,
                       ),
-                    ),
-                  ],
-                );
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const ReuseableText(
+                        "Member",
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                      Column(
+                        children: (state.community?.members
+                                    .where((val) => val.role == "member") ??
+                                [])
+                            .map(
+                          (member) {
+                            return ListTile(
+                              leading: UserAvatar(
+                                uid: member.userId,
+                                size: 35,
+                              ),
+                              title: Text(member.name ?? ""),
+                              trailing: ReuseableText(
+                                member.role ?? "Member",
+                                fontSize: 14,
+                              ),
+                            );
+                          },
+                        ).toList(),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            );
           },
         ),
       ),
